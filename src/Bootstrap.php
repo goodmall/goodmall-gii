def1af2d5ddb66dcb2558f0b1f9d9af93c796ca7
yiqing <yiqing_95@qq.com>
2018-04-22 23:31:17 +0800

    为goodmall 创建gii工具  初步完成 模型，interactor ， repository功能

---
+++ src/Bootstrap.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace year\gii\goodmall;
+
+use yii\base\Application;
+use yii\base\BootstrapInterface;
+
+/**
+ * Class Bootstrap.
+ *
+ * @author yiqing <yiqing_95@qq.com>
+ */
+class Bootstrap implements BootstrapInterface
+{
+    /**
+     * @var string
+     */
+    public $giiBaseUrl = '';
+    /**
+     * Bootstrap method to be called during application bootstrap stage.
+     *
+     * @param Application $app the application currently running
+     */
+    public function bootstrap($app)
+    {
+        if ($app->hasModule('gii')) {
+            if (!isset($app->getModule('gii')->generators['giiant-goodmall-pod'])) {
+                $app->getModule('gii')->generators['gii-goodmall-pod'] = 'year\gii\goodmall\generators\pod\Generator';
+            }
+
+            $gk =     'giiant-goodmall-model' ;
+            if (!isset($app->getModule('gii')->generators[$gk])) {
+                $app->getModule('gii')->generators[$gk] = 'year\gii\goodmall\generators\model\Generator';
+            }
+
+            $gk =     'giiant-goodmall-crud' ;
+            if (!isset($app->getModule('gii')->generators[$gk])) {
+                $app->getModule('gii')->generators[$gk] = 'year\gii\goodmall\generators\crud\Generator';
+            }
+
+            $gk =     'giiant-goodmall-repo' ;
+            if (!isset($app->getModule('gii')->generators[$gk])) {
+                $app->getModule('gii')->generators[$gk] = 'year\gii\goodmall\generators\repository\Generator';
+            }
+
+            if ($app instanceof \yii\console\Application) {
+                $app->controllerMap['giigoodmall-batch'] = 'year\gii\goodmall\commands\BatchController';
+            }
+        }
+
+        // 注入参数
+        if(!empty($this->giiBaseUrl)){
+            $app->params['goodmall.giiBaseUrl'] = $this->giiBaseUrl ;
+           // die($this->giiBaseUrl) ;
+        }
+
+    }
+}
